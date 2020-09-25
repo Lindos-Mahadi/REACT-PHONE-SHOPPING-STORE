4c3fcebbb43137fc427e2379c7d74b5f4fec5e74
lindos-mahadi <lindosmahadi@gmail.com>
2020-09-25 21:30:26 +0600

    first commit

---
+++ src/data.jsx
@@ -0,0 +1,159 @@
+export const storeProducts = [
+    {
+        id: 1,
+        title: "Google Pixel -Black",
+        img: "img/mobile-1.png",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime laboriosam commodi optio asperiores aperiam facere tenetur sapiente repellat cupiditate aut magnam similique voluptatibus, labore minus facilis est ab placeat, tempora obcaecati accusamus accusantium molestiae. Ullam odio quas unde minus atque sed exercitationem veritatis iste, laborum perspiciatis consequatur quia? Asperiores, provident?",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 2,
+        title: "iPHONE",
+        img: "img/mobile-2.png",
+        price: 10,
+        company: "IPHONE",
+        info:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime laboriosam commodi optio asperiores aperiam facere tenetur sapiente repellat cupiditate aut magnam similique voluptatibus, labore minus facilis est ab placeat, tempora obcaecati accusamus accusantium molestiae. Ullam odio quas unde minus atque sed exercitationem veritatis iste, laborum perspiciatis consequatur quia? Asperiores, provident?",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 3,
+        title: "SAMSUNG GALAXY 5",
+        img: "img/mobile-3.png",
+        price: 10,
+        company: "SAMSUNG",
+        info:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime laboriosam commodi optio asperiores aperiam facere tenetur sapiente repellat cupiditate aut magnam similique voluptatibus, labore minus facilis est ab placeat, tempora obcaecati accusamus accusantium molestiae. Ullam odio quas unde minus atque sed exercitationem veritatis iste, laborum perspiciatis consequatur quia? Asperiores, provident?",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 4,
+        title: "HTC GALAXY 5",
+        img: "img/mobile-4.png",
+        price: 10,
+        company: "SAMSUNG",
+        info:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime laboriosam commodi optio asperiores aperiam facere tenetur sapiente repellat cupiditate aut magnam similique voluptatibus, labore minus facilis est ab placeat, tempora obcaecati accusamus accusantium molestiae. Ullam odio quas unde minus atque sed exercitationem veritatis iste, laborum perspiciatis consequatur quia? Asperiores, provident?",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 10,
+        title: "Google Pixel -Black",
+        img: "img/mobile-10.png",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime laboriosam commodi optio asperiores aperiam facere tenetur sapiente repellat cupiditate aut magnam similique voluptatibus, labore minus facilis est ab placeat, tempora obcaecati accusamus accusantium molestiae. Ullam odio quas unde minus atque sed exercitationem veritatis iste, laborum perspiciatis consequatur quia? Asperiores, provident?",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 6,
+        title: "Google Pixel -Black",
+        img: "img/mobile-6.png",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime laboriosam commodi optio asperiores aperiam facere tenetur sapiente repellat cupiditate aut magnam similique voluptatibus, labore minus facilis est ab placeat, tempora obcaecati accusamus accusantium molestiae. Ullam odio quas unde minus atque sed exercitationem veritatis iste, laborum perspiciatis consequatur quia? Asperiores, provident?",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 10,
+        title: "Google Pixel -Black",
+        img: "img/mobile-10.png",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime laboriosam commodi optio asperiores aperiam facere tenetur sapiente repellat cupiditate aut magnam similique voluptatibus, labore minus facilis est ab placeat, tempora obcaecati accusamus accusantium molestiae. Ullam odio quas unde minus atque sed exercitationem veritatis iste, laborum perspiciatis consequatur quia? Asperiores, provident?",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 8,
+        title: "Google Pixel -Black",
+        img: "img/mobile-8.png",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime laboriosam commodi optio asperiores aperiam facere tenetur sapiente repellat cupiditate aut magnam similique voluptatibus, labore minus facilis est ab placeat, tempora obcaecati accusamus accusantium molestiae. Ullam odio quas unde minus atque sed exercitationem veritatis iste, laborum perspiciatis consequatur quia? Asperiores, provident?",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 1,
+        title: "Google Pixel -Black",
+        img: "img/mobile-1.png",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime laboriosam commodi optio asperiores aperiam facere tenetur sapiente repellat cupiditate aut magnam similique voluptatibus, labore minus facilis est ab placeat, tempora obcaecati accusamus accusantium molestiae. Ullam odio quas unde minus atque sed exercitationem veritatis iste, laborum perspiciatis consequatur quia? Asperiores, provident?",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 2,
+        title: "Google Pixel -Black",
+        img: "img/mobile-2.png",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime laboriosam commodi optio asperiores aperiam facere tenetur sapiente repellat cupiditate aut magnam similique voluptatibus, labore minus facilis est ab placeat, tempora obcaecati accusamus accusantium molestiae. Ullam odio quas unde minus atque sed exercitationem veritatis iste, laborum perspiciatis consequatur quia? Asperiores, provident?",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 3,
+        title: "Google Pixel -Black",
+        img: "img/mobile-3.png",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime laboriosam commodi optio asperiores aperiam facere tenetur sapiente repellat cupiditate aut magnam similique voluptatibus, labore minus facilis est ab placeat, tempora obcaecati accusamus accusantium molestiae. Ullam odio quas unde minus atque sed exercitationem veritatis iste, laborum perspiciatis consequatur quia? Asperiores, provident?",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+    {
+        id: 4,
+        title: "Google Pixel -Black",
+        img: "img/mobile-4.png",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime laboriosam commodi optio asperiores aperiam facere tenetur sapiente repellat cupiditate aut magnam similique voluptatibus, labore minus facilis est ab placeat, tempora obcaecati accusamus accusantium molestiae. Ullam odio quas unde minus atque sed exercitationem veritatis iste, laborum perspiciatis consequatur quia? Asperiores, provident?",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+];
+
+export const detailProduct = {
+    id: 1,
+        title: "Google Pixel -Black",
+        img: "img/mobile-1.png",
+        price: 10,
+        company: "GOOGLE",
+        info:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime laboriosam commodi optio asperiores aperiam facere tenetur sapiente repellat cupiditate aut magnam similique voluptatibus, labore minus facilis est ab placeat, tempora obcaecati accusamus accusantium molestiae. Ullam odio quas unde minus atque sed exercitationem veritatis iste, laborum perspiciatis consequatur quia? Asperiores, provident?",
+        inCart: false,
+        count: 0,
+        total: 0
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {BrowserRouter as Router} from "react-router-dom";
+import './App.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
+import {ProductProvider} from "./context";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ProductProvider>
+    <Router>
+      <App />
+    </Router>
+  </ProductProvider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
